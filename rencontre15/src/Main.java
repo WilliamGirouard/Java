e50d52b990b47c640bed33e14f4926e1a1b53567
6245166 <6245166@collegemv.qc.ca>
2024-04-03 08:54:52 -0400

    Ajout d'exemple sur les listes

---
+++ rencontre15/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        parcourirListIterator();
+    }
+    public static void parcourirListIterator() {
+        ArrayList<String> uneListe = new ArrayList<String>();
+        uneListe.add("un");
+        uneListe.add("deux");
+        uneListe.add("trois");
+        uneListe.clear();
+        System.out.println(uneListe.isEmpty());
+    }
+}
